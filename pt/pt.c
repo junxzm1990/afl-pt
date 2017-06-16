4877ab2dee5018529d885ae85e0ee50a0afa1fdf
Jun <junxzm@hotmail.com>
2017-06-16 10:53:41 -0700

    first step: check capability of PT

---
+++ pt/pt.c
@@ -0,0 +1,134 @@
+#include <linux/module.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/cpu.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+#include <linux/gfp.h>
+#include <linux/io.h>
+#include <linux/mm.h>
+#include <linux/uaccess.h>
+#include <linux/sched.h>
+#include <linux/kallsyms.h>
+#include <linux/kprobes.h>
+#include <linux/dcache.h>
+#include <linux/ctype.h>
+#include <linux/syscore_ops.h>
+#include <trace/events/sched.h>
+#include <asm/msr.h>
+#include <asm/processor.h>
+
+#include "pt.h"
+
+
+
+//Data structure for PT capability
+typedef struct pt_cap_struct{
+	bool has_pt;  // if pt is included  
+	bool has_topa;	// if pt supports ToPA
+	bool cr3_match; // if pt can filter based on CR3
+	
+ 	int topa_num;	//number of ToPA entried	
+	int addr_range_num; //number of address ranges for filtering	
+	int psb_freq_mask; //psb packets frequency mast (psb->boundary packets)
+}pt_cap_t; 
+
+pt_cap_t pt_cap = {
+	.has_pt = false,
+	.has_topa = false,
+	.cr3_match = false,
+
+	.topa_num = DEF_TOPA_NUM,
+	.addr_range_num = 2,
+	.psb_freq_mask = 0
+};
+
+//query CPU ID to check capability of PT
+static void query_pt_cap(void){
+	
+	unsigned a, b, c, d;
+	unsigned a1, b1, c1, d1;
+	//CPU id is not high enough to support PT?
+	cpuid(0, &a, &b, &c, &d);
+	if(a < 0x14){
+		pt_cap.has_pt = false;
+		return;
+	}
+	//CPU does not support PT?
+	cpuid_count(0x07, 0, &a, &b, &c, &d);
+
+	if (!(b & BIT(25))) {
+		pt_cap.has_pt = false; 
+		return;
+	}
+	pt_cap.has_pt = true; 
+
+	//CPU has no ToPA for pt?
+	cpuid_count(0x14, 0, &a, &b, &c, &d);
+	pt_cap.has_topa = (c & BIT(0)) ? true : false;  
+
+	//PT supports filtering  by CR3?
+	pt_cap.cr3_match = (b & BIT(0)) ? true : false;
+
+	if (!(c & BIT(1)))
+		pt_cap.topa_num = 1;
+
+	pt_cap.topa_num = min_t(unsigned, pt_cap.topa_num,
+			       (PAGE_SIZE / 8) - 1);
+	a1 = b1 = c1 = d1 = 0;
+	if (a >= 1) cpuid_count(0x14, 1, &a1, &b1, &c1, &d1);
+	if (b & BIT(1)) {
+		pt_cap.psb_freq_mask= (b1 >> 16) & 0xffff;
+		pt_cap.addr_range_num = a1 & 0x3;
+	}
+}
+
+static bool check_pt(void){
+
+	if(!pt_cap.has_pt){
+		printk(KERN_INFO "No PT support\n");
+		return false; 
+	}
+
+	if(!pt_cap.has_topa){
+		printk(KERN_INFO "No ToPA support\n");
+		return false; 
+	}
+
+	if(!pt_cap.cr3_match){
+		printk(KERN_INFO "No filtering based on CR3\n");
+		return false; 
+	}	
+
+	printk("The PT supports %d ToPA entries and %d address ranges for filtering\n", pt_cap.topa_num, pt_cap.addr_range_num);
+
+	return true; 
+}
+
+
+
+//Init pt 
+//1. Check if pt is supported 
+//2. Check capabilities
+//3. Set up hooking point through Linux trace API
+static int __init pt_init(void){
+
+	//query pt_cap
+	query_pt_cap();
+	
+	if(!check_pt()){
+		printk(KERN_INFO "The CPU does not meet requirement\n");
+		return 1;  	
+	}
+	return 0;
+}
+
+
+static void __exit pt_exit(void){
+	printk(KERN_INFO "end test pt test\n");
+}
+
+module_init(pt_init);
+module_exit(pt_exit);
+
+
---
+++ pt/pt.h
@@ -0,0 +1,43 @@
+#ifndef __PT_H__
+#define __PT_H__
+
+#define MSR_IA32_RTIT_OUTPUT_BASE	0x00000560
+#define MSR_IA32_RTIT_OUTPUT_MASK_PTRS	0x00000561
+#define MSR_IA32_RTIT_CTL		0x00000570
+
+#define TRACE_EN	BIT_ULL(0)
+#define CYC_EN		BIT_ULL(1)
+#define CTL_OS		BIT_ULL(2)
+#define CTL_USER	BIT_ULL(3)
+#define PT_ERROR	BIT_ULL(4)
+#define CR3_FILTER	BIT_ULL(7)
+#define TO_PA		BIT_ULL(8)
+#define MTC_EN		BIT_ULL(9)
+#define TSC_EN		BIT_ULL(10)
+#define DIS_RETC	BIT_ULL(11)
+#define BRANCH_EN	BIT_ULL(13)
+#define MTC_MASK	(0xf << 14)
+#define CYC_MASK	(0xf << 19)
+#define PSB_MASK	(0xf << 24)
+#define ADDR0_SHIFT	32
+#define ADDR1_SHIFT	36
+#define ADDR0_MASK	(0xfULL << ADDR0_SHIFT)
+#define ADDR1_MASK	(0xfULL << ADDR1_SHIFT)
+
+#define MSR_IA32_RTIT_STATUS		0x00000571
+#define MSR_IA32_CR3_MATCH		0x00000572
+
+#define TOPA_STOP	BIT_ULL(4)
+#define TOPA_INT	BIT_ULL(2)
+#define TOPA_END	BIT_ULL(0)
+
+#define TOPA_SIZE_SHIFT 6
+#define MSR_IA32_ADDR0_START		0x00000580
+#define MSR_IA32_ADDR0_END		0x00000581
+#define MSR_IA32_ADDR1_START		0x00000582
+#define MSR_IA32_ADDR1_END		0x00000583
+
+#define DEF_TOPA_NUM			0x02
+
+#endif
+
