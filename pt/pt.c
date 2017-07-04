3580d38cbfad6b75a0f91c92dc7f65d148e5f53a
yaoh <yaohui.c@partner.samsung.com>
2017-07-04 13:51:08 -0700

    add typedef for several func pointers, and suppress the type-cast warnings

--- pt/pt.c
+++ pt/pt.c
@@ -37,7 +37,8 @@ MODULE_PARM_DESC(kallsyms_lookup_name_ptr, "Set address of function kallsyms_loo
 
 
 
-unsigned long (*ksyms_func)(const char *name) = NULL;
+/* unsigned long (*ksyms_func)(const char *name) = NULL; */
+ksyms_func_ptr_ty ksyms_func = NULL;
 static void pt_recv_msg(struct sk_buff *skb);
 
 #define TOPA_ENTRY(_base, _size, _stop, _intr, _end) (struct topa_entry) { \
@@ -361,7 +362,8 @@ static void probe_trace_exit(void * ignore, struct task_struct *tsk){
 
 static bool set_trace_point(void){
 
-	int (*trace_probe_ptr)(struct tracepoint *tp, void *probe, void *data);
+	/* int (*trace_probe_ptr)(struct tracepoint *tp, void *probe, void *data); */
+  trace_probe_ptr_ty trace_probe_ptr;
 	if(!kallsyms_lookup_name_ptr){
 		printk(KERN_INFO "Please specify the address of kallsyms_lookup_name_ptr");
     return false; //if we don't stop here when kernel tries to access an invalid address
@@ -388,7 +390,7 @@ static bool set_trace_point(void){
 	if(!exit_tp)
 		return false; 
 
-       trace_probe_ptr = ksyms_func("tracepoint_probe_register");
+  trace_probe_ptr = (trace_probe_ptr_ty)ksyms_func("tracepoint_probe_register");
 
 	if(!trace_probe_ptr)
 		return false;
@@ -402,14 +404,15 @@ static bool set_trace_point(void){
 
 static void release_trace_point(void){
 	
-	int (*trace_release_ptr)(struct tracepoint *tp, void *probe, void *data); 
+	/* int (*trace_release_ptr)(struct tracepoint *tp, void *probe, void *data);  */
+  trace_release_ptr_ty trace_release_ptr;
 
 	if(!ksyms_func){
 		printk(KERN_INFO "FFF Cannot unregister trace points!!!");
 		return;	
 	}
 
-	trace_release_ptr = ksyms_func("tracepoint_probe_unregister"); 
+	trace_release_ptr = (trace_release_ptr_ty)ksyms_func("tracepoint_probe_unregister"); 
 	if(!trace_release_ptr){
 		printk(KERN_INFO "Cannot unregister trace points!!!");
 		return;
@@ -537,7 +540,7 @@ static int __init pt_init(void){
 		printk(KERN_INFO "Please specify the address to kallsyms_lookup_name\n");
 	}
 
-	ksyms_func = kallsyms_lookup_name_ptr; 
+	ksyms_func = (ksyms_func_ptr_ty)kallsyms_lookup_name_ptr; 
 	nlt.nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &nlt.cfg);
 	return 0;
 }
