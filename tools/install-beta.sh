1cd76f25c9365b5de43d9a3ca864ed3077971044
evanmak <yaohchen@cs.stonybrook.edu>
2019-06-03 16:15:01 -0400

    Update install-beta.sh

--- tools/install-beta.sh
+++ tools/install-beta.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 pushd pt
-make
+make -j$(nproc)
 # require sudo privilege to insert PT module
 ./reinstall_ptmod.sh
 popd
 
 # Build the customized AFL
 pushd afl-2.42b
-make
+make -j$(nproc)
 popd
 
 ## Build elfpatcher
 pushd afl-2.42b/pt_mode/elfpatcher
 ./bootstrap.sh
 ./configure
-make
+make -j$(nproc)
 popd
 
 ## Build pt_proxy
 pushd afl-2.42b/pt_mode/pt_proxy
-make
+make -j$(nproc)
 popd
 
 ## Build customized ld
@@ -28,7 +28,7 @@ pushd afl-2.42b/pt_mode/glibc-2.19
 mkdir build
 cd build
 ../configure
-make -j8
+make -j$(nproc)
 cd ../
 popd
 
@@ -37,7 +37,7 @@ pushd afl-2.42b/test_progs/binutils-2.29
 mkdir build
 cd build
 ../configure --enable-shared=no --enable-static=yes
-make -j8
+make -j$(nproc)
 cd ../
 popd
 
